7c22fb15d2dde88d573ef55a934d60ad43a60938
rouf <1505097.ar@ugrad.buet.ac.bd>
2023-05-31 22:14:31 -0700

    improved performance in terms of memory and also reduced time to half

--- src/leetcode/current/beginners_guide/running_sum_of_1_d_array/Solution.java
+++ src/leetcode/current/beginners_guide/running_sum_of_1_d_array/Solution.java
@@ -7,17 +7,11 @@ public class Solution {
     // https://leetcode.com/problems/running-sum-of-1d-array/
     public int[] runningSum(int[] nums) {
         // it's a cumulative sum actually
-        List<Integer> sum = new ArrayList<>();
         int cumulative_sum = 0;
-        for(int n : nums) {
-            cumulative_sum += n;
-            sum.add(cumulative_sum);
+        for(int i = 0; i < nums.length; i++) {
+            cumulative_sum += nums[i];
+            nums[i] = cumulative_sum;
         }
-
-        int[] ans = new int[sum.size()];
-        for(int i = 0; i < sum.size(); i++) {
-            ans[i] = sum.get(i);
-        }
-        return ans;
+        return nums;
     }
 }
