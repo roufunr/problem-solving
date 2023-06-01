921e461391752d8dd0f9d683f7487dc752964e9b
rouf <1505097.ar@ugrad.buet.ac.bd>
2023-06-01 10:17:38 -0700

    fizz buzz problem

---
+++ src/leetcode/current/beginners_guide/fizz_buzz/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.current.beginners_guide.fizz_buzz;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().fizzBuzz(16);
+    }
+}
---
+++ src/leetcode/current/beginners_guide/fizz_buzz/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.current.beginners_guide.fizz_buzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    // https://leetcode.com/problems/fizz-buzz/
+    public List<String> fizzBuzz(int n) {
+        String fizz = "Fizz";
+        String buzz = "Buzz";
+        List<String> output = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(i % 15 == 0) {
+                output.add(fizz + buzz);
+            } else if(i % 3 == 0) {
+                output.add(fizz);
+            } else if(i % 5 == 0) {
+                output.add(buzz);
+            } else {
+                output.add("" + i);
+            }
+        }
+        return output;
+    }
+}
