473601850b322f649014077b1bb683e1ab89469e
roufunr <rouf@nevada.unr.edu>
2023-05-31 20:40:37 -0700

    previously solved codes are here

---
+++ src/leetcode/binary_search/rotated_sorted_array_III/Runner.java
@@ -0,0 +1,10 @@
+package leetcode.binary_search.rotated_sorted_array_III;
+
+
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] arr = {10, 1, 10, 10, 10};
+        System.out.println(new Solution().findMin(arr));
+    }
+}
---
+++ src/leetcode/binary_search/search_for_a_range/Runner.java
@@ -0,0 +1,10 @@
+package leetcode.binary_search.search_for_a_range;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = {2, 2};
+
+        int target = 2;
+        new Solution().searchRange(nums, target);
+    }
+}
