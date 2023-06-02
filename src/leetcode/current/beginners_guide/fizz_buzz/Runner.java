96167e0ff044d2d6d27ff3a7519f87e28ef7d5af
rouf <1505097.ar@ugrad.buet.ac.bd>
2023-06-01 23:08:29 -0700

    fizz buzz problem solved

--- src/leetcode/current/beginners_guide/fizz_buzz/Runner.java
+++ src/leetcode/current/beginners_guide/fizz_buzz/Runner.java
@@ -2,6 +2,7 @@ package leetcode.current.beginners_guide.fizz_buzz;
 
 public class Runner {
     public static void main(String[] args) {
-        new Solution().fizzBuzz(16);
+
+        new Solution().fizzBuzz(17);
     }
 }
