79fd5c928606bce99ef38afb5f2e48505cd49ece
roufunr <rouf@nevada.unr.edu>
2023-06-02 19:07:02 -0700

    solved steps count problem updated version

--- src/leetcode/current/beginners_guide/number_of_steps_to_reduce_a_number_to_zero/Solution.java
+++ src/leetcode/current/beginners_guide/number_of_steps_to_reduce_a_number_to_zero/Solution.java
@@ -5,11 +5,7 @@ public class Solution {
     public int numberOfSteps(int num) {
         int steps = 0;
         while (num > 0) {
-            if(num % 2 == 0) {
-                num = num / 2;
-            } else {
-                num = num - 1;
-            }
+            num = num % 2 == 0 ? (num / 2) : (num - 1);
             steps++;
         }
         return steps;
