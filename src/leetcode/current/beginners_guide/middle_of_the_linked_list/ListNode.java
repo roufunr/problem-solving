7ef59298b1c69aa592fa471467577659c83c86de
roufunr <rouf@nevada.unr.edu>
2023-06-07 23:43:58 -0700

    solved middle of the linked list

---
+++ src/leetcode/current/beginners_guide/middle_of_the_linked_list/ListNode.java
@@ -0,0 +1,9 @@
+package leetcode.current.beginners_guide.middle_of_the_linked_list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/leetcode/current/beginners_guide/middle_of_the_linked_list/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.current.beginners_guide.middle_of_the_linked_list;
+
+public class Solution {
+    private int linkedListLength(ListNode head) {
+        int l = 0;
+        ListNode temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            l++;
+        }
+        return l;
+    }
+    public ListNode middleNode(ListNode head) {
+        int len = linkedListLength(head);
+        int midIdx = (int) Math.floor(len / 2.0);
+        ListNode temp = head;
+        int i = 0;
+        while(i < midIdx) {
+            temp = temp.next;
+            i++;
+        }
+        return temp;
+    }
+}
