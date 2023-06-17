ee1f47c81bd160d757858432d1306b22efc2e60a
roufunr <rouf@nevada.unr.edu>
2023-06-16 21:09:14 -0700

    ok. but not efficient

---
+++ src/leetcode/current/arrays/check_if_N_and_its_double_exists/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.current.arrays.check_if_N_and_its_double_exists;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] arr = {10,2,5,3};
+        new Solution().checkIfExist(arr);
+    }
+}
---
+++ src/leetcode/current/arrays/check_if_N_and_its_double_exists/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.current.arrays.check_if_N_and_its_double_exists;
+
+public class Solution {
+    // https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3250/
+    public boolean checkIfExist(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length; j++) {
+                if (j!=i && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
