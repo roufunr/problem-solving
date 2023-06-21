1e64eb43a7a730b95ecc7fb6f289e0d5247a1d67
roufunr <rouf@nevada.unr.edu>
2023-06-21 01:05:50 -0700

    not efficient

---
+++ src/leetcode/current/arrays/height_checker/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.current.arrays.height_checker;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    // https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+    public int heightChecker(int[] heights) {
+        int[] sortedHeight = new int[heights.length];
+        for(int i = 0; i < heights.length; i++) {
+            sortedHeight[i] = heights[i];
+        }
+        Arrays.sort(sortedHeight);
+        int misplaced = 0;
+        for(int i = 0; i < heights.length; i++) {
+            if(heights[i] != sortedHeight[i]) {
+                misplaced++;
+            }
+        }
+        return misplaced;
+    }
+}
