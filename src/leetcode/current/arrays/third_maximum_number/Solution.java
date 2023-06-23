d0525d937943320206b7ea8414e99390d6e269fa
roufunr <rouf@nevada.unr.edu>
2023-06-23 01:05:50 -0700

    third maxima partially solved in efficient way

--- src/leetcode/current/arrays/third_maximum_number/Solution.java
+++ src/leetcode/current/arrays/third_maximum_number/Solution.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Solution {
     // https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3231/
-    public int thirdMax(int[] nums) {
+    public int thirdMaxV1(int[] nums) {
         Arrays.sort(nums);
         int maxima = nums[nums.length - 1];
         int distinctCount = 1;
@@ -15,5 +15,24 @@ public class Solution {
             }
         }
         return distinctCount < 3 ? nums[nums.length - 1] : maxima;
+    }
+
+    public int thirdMax(int[] nums) {
+        int[] maxArr = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+
+        for(int i = 0; i < nums.length; i++) {
+            boolean found = false;
+            for(int j = 0; j < maxArr.length; j++) {
+                if(maxArr[j] == nums[i]) {
+                    found = true;
+                    break;
+                }
+            }
+            if(found == true) continue;
+
+            maxArr[0] = nums[i];
+            Arrays.sort(maxArr);
+        }
+        return maxArr[1] == Integer.MIN_VALUE ? maxArr[3] : maxArr[1];
     }
 }
