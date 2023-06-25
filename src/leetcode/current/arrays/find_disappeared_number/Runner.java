1ffe2923e2beb09b47d138b7f5f957b53244da71
roufunr <rouf@nevada.unr.edu>
2023-06-24 23:19:57 -0700

    efficient solution

---
+++ src/leetcode/current/arrays/find_disappeared_number/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.current.arrays.find_disappeared_number;
+
+public class Runner {
+    public static void main(String[] args) {
+        int [] nums = {1, 1};
+        new Solution().findDisappearedNumbers(nums);
+    }
+}
---
+++ src/leetcode/current/arrays/find_disappeared_number/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.current.arrays.find_disappeared_number;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    // https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            nums[Math.abs(nums[i]) - 1] = nums[Math.abs(nums[i]) - 1] < 0
+                    ? nums[Math.abs(nums[i]) - 1]
+                    : nums[Math.abs(nums[i]) - 1] * (-1);
+        }
+        List<Integer> disappearedNumbers = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                disappearedNumbers.add(i+1);
+            }
+        }
+        return disappearedNumbers;
+    }
+}
