1c0aca8c5fd4499048ffd2c940eb4eb421bcc70c
roufunr <rouf@nevada.unr.edu>
2023-07-04 14:13:17 -0700

    efficient solution

--- src/leetcode/two_thousand_23/linked_list/odd_even_list/Runner.java
+++ src/leetcode/two_thousand_23/linked_list/odd_even_list/Runner.java
@@ -5,11 +5,11 @@ package leetcode.two_thousand_23.linked_list.odd_even_list;
 public class Runner {
     public static void main(String[] args) {
         ListNode next = new ListNode(10);
-        for(int i = 9; i > 0; i--) {
+        for(int i = 1; i > 0; i--) {
             ListNode node = new ListNode(i, next);
             next = node;
         }
-        ListNode head = next;
+        ListNode head = null;
         new Solution().oddEvenList(head);
     }
 }
--- src/leetcode/two_thousand_23/linked_list/odd_even_list/Solution.java
+++ src/leetcode/two_thousand_23/linked_list/odd_even_list/Solution.java
@@ -3,6 +3,27 @@ package leetcode.two_thousand_23.linked_list.odd_even_list;
 public class Solution {
     // https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1208/
     public ListNode oddEvenList(ListNode head) {
-
+            ListNode oddHead = head != null ? head.next : null;
+            ListNode even = head;
+            ListNode odd = oddHead;
+            if(even == null || odd == null) {
+                return head;
+            }
+            while(even != null || odd!= null) {
+                if(even != null && even.next != null) {
+                    even.next = even.next.next;
+                }
+                if(odd != null && odd.next != null) {
+                    odd.next = odd.next.next;
+                }
+                even = even !=null ?  even.next : null;
+                odd =  odd !=null ?  odd.next : null;
+            }
+            even = head;
+            while(even.next != null) {
+                even = even.next;
+            }
+            even.next = oddHead;
+            return head;
     }
 }
