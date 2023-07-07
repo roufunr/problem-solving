f5cb90deae446d240b5ba19f2aada791fc868351
roufunr <rouf@nevada.unr.edu>
2023-07-07 00:00:19 -0700

    add at head, add at tail and find by indices method are working

--- src/leetcode/two_thousand_23/linked_list/design_doubly_linked_list/MyLinkedList.java
+++ src/leetcode/two_thousand_23/linked_list/design_doubly_linked_list/MyLinkedList.java
@@ -3,33 +3,73 @@ package leetcode.two_thousand_23.linked_list.design_doubly_linked_list;
 class Node {
     public int val;
     public Node next;
+    public Node prev;
     public Node(int val) {
         this.val = val;
         this.next = null;
+        this.prev = null;
     }
 }
 
 class MyLinkedList {
+    // https://leetcode.com/explore/learn/card/linked-list/210/doubly-linked-list/1294/
     private Node head;
+    private Node tail;
     private int size;
     public MyLinkedList() {
         head = null;
+        tail = null;
         size = 0;
     }
+
     public int get(int index) {
-
+        if(head == null || index < 0 || index >= size) {
+            return -1;
+        }
+        Node itr = head;
+        int i = 0;
+        while(itr != null) {
+            if(index == i) {
+                return itr.val;
+            }
+            itr = itr.next;
+            i++;
+        }
+        return -1;
     }
+
     public void addAtHead(int val) {
-
+        Node newNode = new Node(val);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+            size++;
+            return;
+        }
+        head.prev = newNode;
+        newNode.next = head;
+        head = newNode;
+        size++;
     }
+
     public void addAtTail(int val) {
-
+        Node newNode = new Node(val);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+            size++;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        size++;
     }
+
     public void addAtIndex(int index, int val) {
 
-
-
     }
+
     public void deleteAtIndex(int index) {
 
     }
---
+++ src/leetcode/two_thousand_23/linked_list/design_doubly_linked_list/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.two_thousand_23.linked_list.design_doubly_linked_list;
+
+public class Runner {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+    }
+}
