88250f18218638b5afbe36c126c6ffd9416e54f9
roufunr <rouf@nevada.unr.edu>
2023-07-08 20:54:41 -0700

    efficient solution

---
+++ src/leetcode/two_thousand_23/weekly_contest/wc_353/find_the_maximum_acheievable_number/Solution.java
@@ -0,0 +1,8 @@
+package leetcode.two_thousand_23.weekly_contest.wc_353.find_the_maximum_acheievable_number;
+
+public class Solution {
+    // https://leetcode.com/contest/weekly-contest-353/problems/find-the-maximum-achievable-number/
+    public int theMaximumAchievableX(int num, int t) {
+        return num + (2 * t);
+    }
+}
