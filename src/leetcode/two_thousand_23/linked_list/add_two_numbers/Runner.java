08e67613e425708de9a86b52ecada4253a565a08
roufunr <rouf@nevada.unr.edu>
2023-07-13 19:05:19 -0700

    solve linked list addition

--- src/leetcode/two_thousand_23/linked_list/add_two_numbers/Runner.java
+++ src/leetcode/two_thousand_23/linked_list/add_two_numbers/Runner.java
@@ -1,4 +1,22 @@
 package leetcode.two_thousand_23.linked_list.add_two_numbers;
 
+
+
 public class Runner {
+    public static void main(String[] args) {
+        ListNode next1 = new ListNode(9);
+        for(int i = 8; i >= 6; i--) {
+            ListNode node = new ListNode(i, next1);
+            next1 = node;
+        }
+        ListNode head1 = next1;
+
+        ListNode next2 = new ListNode(8);
+        for(int i = 7; i >= 4; i--) {
+            ListNode node = new ListNode(i, next2);
+            next2 = node;
+        }
+        ListNode head2 = next2;
+        new Solution().addTwoNumbers(head1, head2);
+    }
 }
--- src/leetcode/two_thousand_23/linked_list/add_two_numbers/Solution.java
+++ src/leetcode/two_thousand_23/linked_list/add_two_numbers/Solution.java
@@ -1,7 +1,38 @@
 package leetcode.two_thousand_23.linked_list.add_two_numbers;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        ListNode itr1 = l1;
+        ListNode itr2 = l2;
+        ListNode itr = null;
+        ListNode head = null;
+        int remainder = 0;
+        while(itr1 != null || itr2 != null) {
+            int num1 = itr1 != null ? itr1.val : 0;
+            int num2 = itr2 != null ? itr2.val : 0;
+            int digit = num1 + num2 + remainder;
+            remainder = digit / 10;
+            digit = digit % 10;
+            ListNode newNode = new ListNode(digit);
+            if(head == null) {
+                head = newNode;
+                itr = head;
+            } else {
+                itr.next = newNode;
+                itr = itr.next;
+            }
+            itr1 = itr1 != null ? itr1.next : null;
+            itr2 = itr2 != null ? itr2.next : null;
+        }
+        if(remainder > 0) {
+            ListNode newNode = new ListNode(remainder);
+            itr.next = newNode;
+            itr = itr.next;
+        }
+        return head;
     }
 }
