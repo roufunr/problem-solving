a7d0ade581a3bfd9b4b4074f2f4118d254b5c221
roufunr <rouf@nevada.unr.edu>
2023-07-16 19:25:16 -0700

    solved maximum beauty problem efficiently

--- src/leetcode/two_thousand_23/weekly_contest/wc_354/maximum_beauty/Solution.java
+++ src/leetcode/two_thousand_23/weekly_contest/wc_354/maximum_beauty/Solution.java
@@ -3,47 +3,25 @@ package leetcode.two_thousand_23.weekly_contest.wc_354.maximum_beauty;
 import java.util.Arrays;
 
 public class Solution {
-    private int findMaxOverlaps(int[][] pairs) {
-        int n = pairs.length;
-        int[] startPoints = new int[n];
-        int[] endPoints = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            startPoints[i] = pairs[i][0];
-            endPoints[i] = pairs[i][1];
-        }
-
-        Arrays.sort(startPoints);
-        Arrays.sort(endPoints);
-
-        int currentOverlap = 0;
-        int maxOverlap = 0;
-        int startPtr = 0;
-        int endPtr = 0;
-
-        while (startPtr < n) {
-            if (startPoints[startPtr] <= endPoints[endPtr]) {
-                currentOverlap++;
-                maxOverlap = Math.max(maxOverlap, currentOverlap);
-                startPtr++;
+    // https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation/
+    public int maximumBeauty(int[] nums, int k) {
+        Arrays.sort(nums);
+        int startIdx = 0;
+        int endIdx = 0;
+        int max = 0;
+        while(startIdx < nums.length && endIdx < nums.length) {
+            int diff = nums[endIdx] - nums[startIdx];
+            if(diff <= (2 * k)) {
+                max = endIdx - startIdx + 1 > max ? endIdx - startIdx + 1 : max;
+                endIdx++;
             } else {
-                currentOverlap--;
-                endPtr++;
+                startIdx++;
+            }
+            if(startIdx > endIdx) {
+                endIdx = startIdx;
             }
         }
-
-        return maxOverlap;
-    }
-    public int maximumBeauty(int[] nums, int k) {
-        // unsolved
-        int[][] pairs = new int[nums.length][2];
-        int itr = 0;
-        for(int n : nums) {
-            int[] pair = {n - k, n + k};
-            pairs[itr] = pair;
-        }
-        int maxOverlaps = findMaxOverlaps(pairs);
-        return maxOverlaps;
+        return max;
     }
 
 }
