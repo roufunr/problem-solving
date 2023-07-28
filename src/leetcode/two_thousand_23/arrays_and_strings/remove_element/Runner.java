f893b3bc3b1a620483b9e37773e64eb06cb0f8c5
roufunr <rouf@nevada.unr.edu>
2023-07-28 00:30:42 -0700

    solved remove problem

---
+++ src/leetcode/two_thousand_23/arrays_and_strings/remove_element/Runner.java
@@ -0,0 +1,9 @@
+package leetcode.two_thousand_23.arrays_and_strings.remove_element;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        new Solution().removeElement(nums, val);
+    }
+}
