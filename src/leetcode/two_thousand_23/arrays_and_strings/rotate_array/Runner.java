9ff95d4819975d1622a7706436d746415f351b72
roufunr <rouf@nevada.unr.edu>
2023-07-28 23:48:51 -0700

    solved rotate problem

---
+++ src/leetcode/two_thousand_23/arrays_and_strings/rotate_array/Runner.java
@@ -0,0 +1,9 @@
+package leetcode.two_thousand_23.arrays_and_strings.rotate_array;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = {1};
+        int k = 3;
+        new Solution().rotate(nums, k);
+    }
+}
