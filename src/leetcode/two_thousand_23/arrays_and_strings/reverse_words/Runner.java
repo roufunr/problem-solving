fb36622528a6d453da4482accca590a0bac8a7bb
roufunr <rouf@nevada.unr.edu>
2023-07-30 21:11:41 -0700

    solved word reverse in a sentence

---
+++ src/leetcode/two_thousand_23/arrays_and_strings/reverse_words/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.two_thousand_23.arrays_and_strings.reverse_words;
+
+public class Runner {
+    public static void main(String[] args) {
+        String s = "a good   example";
+        new Solution().reverseWords(s);
+    }
+}
