cd504e48e78fa757b26ad93a39413d604a2bf070
roufunr <rouf@nevada.unr.edu>
2023-08-11 23:52:00 -0700

    added link

--- src/leetcode/two_thousand_23/hash_table/contains_duplicate_II/Solution.java
+++ src/leetcode/two_thousand_23/hash_table/contains_duplicate_II/Solution.java
@@ -2,6 +2,7 @@ package leetcode.two_thousand_23.hash_table.contains_duplicate_II;
 import java.util.*;
 
 public class Solution {
+    // https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1121/
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
