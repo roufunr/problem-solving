39ebfbdbb7690f43f696f042d53f2fd8dfa00d6b
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2023-08-12 06:48:13 +0000

    Solved contains duplicate II

---
+++ src/leetcode/two_thousand_23/hash_table/contains_duplicate_II/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.two_thousand_23.hash_table.contains_duplicate_II;
+import java.util.*;
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int prevIdx = map.get(nums[i]);
+                if(i - prevIdx <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
+
