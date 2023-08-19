545a8415ba8f7d8902d1084abd69ad73bbd7dade
roufunr <rouf@nevada.unr.edu>
2023-08-19 06:50:34 +0000

    solved non repeated sub string problem

---
+++ src/leetcode/two_thousand_23/hash_table/longest_subtsring_without_repetiton/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.two_thousand_23.hash_table.longest_subtsring_without_repetiton;
+
+public class Runner {
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        new Solution().lengthOfLongestSubstring(s);
+    }    
+}
---
+++ src/leetcode/two_thousand_23/hash_table/longest_subtsring_without_repetiton/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.two_thousand_23.hash_table.longest_subtsring_without_repetiton;
+import java.util.*;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int right = 0;
+        int maxSubstringLen = Integer.MIN_VALUE;
+        Set<Character> explored = new HashSet<>();
+        while(right < s.length()) {
+            int windowSize = right - left + 1;
+            maxSubstringLen = windowSize > maxSubstringLen ? windowSize : maxSubstringLen;
+            if(explored.contains(s.charAt(right))) {
+                left = right;
+                explored = new HashSet<>();
+            } else {
+                explored.add(s.charAt(right));
+            }
+            right++;
+        }
+        return maxSubstringLen;
+    }
+}
