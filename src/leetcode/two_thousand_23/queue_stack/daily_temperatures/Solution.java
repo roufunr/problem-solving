8d8be2908c9652a018dad63560e1b9fb3cf27a26
roufunr <rouf@nevada.unr.edu>
2023-08-31 21:45:29 +0000

    refactored the solution of daily temperature

--- src/leetcode/two_thousand_23/queue_stack/daily_temperatures/Solution.java
+++ src/leetcode/two_thousand_23/queue_stack/daily_temperatures/Solution.java
@@ -3,11 +3,10 @@ import java.util.*;
 
 public class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        // [73,74,75,71,69,72,76,73]
-        // [1,1,4,2,1,1,0,0]
         Stack<Integer> stack = new  Stack<>();
         int[] ans = new int[temperatures.length];
-        for(int i =0; i < temperatures.length; i++) {
+        stack.push(0);
+        for(int i = 1; i < temperatures.length; i++) {
             while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
                 int j = stack.pop();
                 ans[j] = i -j;
