b0d2d457f5b85cf0c7ca428240b8ee0b64192a9d
roufunr <rouf@nevada.unr.edu>
2023-09-02 23:00:37 -0700

    solved number of islands using DFS

--- src/leetcode/two_thousand_23/queue_stack/number_of_islands/Solution.java
+++ src/leetcode/two_thousand_23/queue_stack/number_of_islands/Solution.java
@@ -2,6 +2,7 @@ package leetcode.two_thousand_23.queue_stack.number_of_islands;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Solution {
     private void startBFS(int i, int j, char[][] grid, boolean[][] visited) {
@@ -36,6 +37,37 @@ public class Solution {
 
         }
     }
+
+    private void startDFS(int i, int j, char[][] grid, boolean[][] visited) {
+        Stack<String> stack = new Stack<>();
+        stack.push(i + "_" + j);
+        visited[i][j] = true;
+        while(!stack.isEmpty()) {
+            String[] position = stack.pop().split("_");
+            int x = Integer.parseInt(position[0]);
+            int y = Integer.parseInt(position[1]);
+
+            if(x-1 >= 0 && grid[x-1][y] == '1' && visited[x-1][y] == false) { // left
+                stack.push((x-1) + "_" + y);
+                visited[x-1][y] = true;
+            }
+
+            if(x+1 < grid.length && grid[x+1][y] == '1' && visited[x+1][y] == false) { //right
+                stack.push((x+1) + "_" + y);
+                visited[x+1][y] = true;
+            }
+
+            if(y-1 >= 0 && grid[x][y-1] == '1' && visited[x][y-1] == false) { // up
+                stack.push((x) + "_" + (y-1));
+                visited[x][y-1] = true;
+            }
+
+            if(y + 1 < grid[0].length && grid[x][y + 1] == '1' && visited[x][y + 1] == false) {
+                stack.push((x) + "_" + (y + 1));
+                visited[x][y + 1] = true;
+            }
+        }
+    }
     public int numIslands(char[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         int islandCount = 0;
@@ -43,7 +75,7 @@ public class Solution {
             for (int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == '1' && visited[i][j] == false) {
                     islandCount++;
-                    startBFS(i, j, grid, visited);
+                    startDFS(i, j, grid, visited);
                 }
             }
         }
