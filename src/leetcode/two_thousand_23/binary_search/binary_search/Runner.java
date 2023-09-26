12f85fc8b8b68b2a9ebdc20ee63d68383d3c2404
roufunr <rouf@nevada.unr.edu>
2023-09-26 03:44:59 -0700

    solved binary search basic

---
+++ src/leetcode/two_thousand_23/binary_search/binary_search/Runner.java
@@ -0,0 +1,11 @@
+package leetcode.two_thousand_23.binary_search.binary_search;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] arr = {
+                5
+        };
+        int target = 5;
+        new Solution().search(arr, target);
+    }
+}
---
+++ src/leetcode/two_thousand_23/binary_search/binary_search/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.two_thousand_23.binary_search.binary_search;
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
