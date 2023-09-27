3c1514914c08ef3533af67849efa15a23182f8e4
roufunr <rouf@nevada.unr.edu>
2023-09-27 00:49:30 -0700

    solved sqrt

--- src/leetcode/two_thousand_23/binary_search/sqrt_x/Runner.java
+++ src/leetcode/two_thousand_23/binary_search/sqrt_x/Runner.java
@@ -2,6 +2,6 @@ package leetcode.two_thousand_23.binary_search.sqrt_x;
 
 public class Runner {
     public static void main(String[] args) {
-
+        new Solution().mySqrt(2147395599);
     }
 }
--- src/leetcode/two_thousand_23/binary_search/sqrt_x/Solution.java
+++ src/leetcode/two_thousand_23/binary_search/sqrt_x/Solution.java
@@ -1,4 +1,24 @@
 package leetcode.two_thousand_23.binary_search.sqrt_x;
 
 public class Solution {
+    // need to handle memory overflow that is the most challenging part
+    public int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        long left = 1;
+        long right = x/2;
+        long X = (long)x;
+        while(left <= right) {
+            long mid = left + (right - left) / 2;
+            if(mid * mid == X) {
+                return (int)mid;
+            } else if (mid * mid > X) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return (int)right;
+    }
 }
