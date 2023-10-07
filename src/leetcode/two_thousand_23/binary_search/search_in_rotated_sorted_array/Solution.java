1a8396af2638505775999c6b52b21123a50c0542
roufunr <rouf@nevada.unr.edu>
2023-10-07 07:45:59 +0000

    added some intuition

--- src/leetcode/two_thousand_23/binary_search/search_in_rotated_sorted_array/Solution.java
+++ src/leetcode/two_thousand_23/binary_search/search_in_rotated_sorted_array/Solution.java
@@ -1,6 +1,14 @@
 package leetcode.two_thousand_23.binary_search.search_in_rotated_sorted_array;
 
 public class Solution {
+    // intuition
+    // here we're comparing the mid with the last element
+    // most important part of this program is first if statement
+    // if the mid is clearly greater than the last element then 
+    // we're sure that the pivot will be somehere in the right half
+    // and as we're taking the left idx as the final pivot that is why
+    // we're focusing on the update of left idx value
+    
     private int findPivot(int[] nums) {
         // finding pivot is the main task
         int left = 0;
