c5dd74e2c3c6f685c4c205a665ff7f0150a9a899
roufunr <rouf@nevada.unr.edu>
2023-10-23 06:44:38 +0000

    first bad version solved

--- src/leetcode/two_thousand_23/binary_search/first_bad_version/Solution.java
+++ src/leetcode/two_thousand_23/binary_search/first_bad_version/Solution.java
@@ -13,23 +13,23 @@ public class Solution {
         }
     }
     public int firstBadVersion(int n) {
-        int left = 1;
-        int right = n;
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-            if(isBadVersion(mid)) {
+        int l = 1;
+        int r = n;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(!isBadVersion(mid)) {
                 if(isBadVersion(mid + 1)) {
-                    right = mid;
+                    return mid + 1;
                 } else {
-                    return mid;
+                    l = mid + 1;
                 }
             } else {
-                left = mid + 1;
+                r = mid;
             }
         }
-        if(isBadVersion(left)) {
-            return left;
+        if(!isBadVersion(l)) {
+            return l + 1;
         }
-        return 1;
+        return l;
     }
 }
