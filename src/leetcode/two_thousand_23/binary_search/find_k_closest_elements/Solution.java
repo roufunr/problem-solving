eef3aef4a2ba2d485c404038622f6a6094d6ce6f
roufunr <rouf@nevada.unr.edu>
2023-10-27 14:31:06 -0700

    k-closest elements has been started

---
+++ src/leetcode/two_thousand_23/binary_search/find_k_closest_elements/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.two_thousand_23.binary_search.find_k_closest_elements;
+import java.util.ArrayList;
+import java.util.List;
+public class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
