be884915fcc513c000de85c27b3354017f75dc05
roufunr <rouf@nevada.unr.edu>
2023-11-27 06:42:00 -0800

    wrote the intuition of this problem

--- src/leetcode/two_thousand_23/binary_search/find_peak_element/Solution.java
+++ src/leetcode/two_thousand_23/binary_search/find_peak_element/Solution.java
@@ -2,20 +2,25 @@ package leetcode.two_thousand_23.binary_search.find_peak_element;
 
 public class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums == null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return -1;
         }
         int left = 0;
         int right = nums.length - 1;
-        while(left < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] > nums[mid + 1]) {
+            if (nums[mid] > nums[mid + 1]) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        // freezing condition is when left reaches to some local maxima where
+        // where right may be reached to that point or previously pointed at that point
+        // this is the break condition of the loop
+        // so technically left or right is the peak element
+        // the intuition is when both reach same point that infers the local maxima
+        return right;
     }
 }
 
