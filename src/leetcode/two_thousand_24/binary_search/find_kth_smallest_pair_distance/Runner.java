83c6b206974a4db44b955ed65485a02c146f141d
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-01-23 19:20:47 +0000

    added some comment

--- src/leetcode/two_thousand_24/binary_search/find_kth_smallest_pair_distance/Runner.java
+++ src/leetcode/two_thousand_24/binary_search/find_kth_smallest_pair_distance/Runner.java
@@ -1,8 +1,18 @@
 package leetcode.two_thousand_24.binary_search.find_kth_smallest_pair_distance;
+
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
-        int[] nums = {1,3,1};
+        int[] nums = { 1, 3, 1, 1, 3 };
         int k = 1;
         System.out.println(new Solution().smallestDistancePair(nums, k));
+        // Arrays.sort(nums);
+        // int lo = 0;
+        // int hi = nums[nums.length - 1] - nums[0];
+        // for (int i = lo; i <= hi; i++) {
+        // System.out.println("dist: " + i + " --> " + new Solution().getCount(nums,
+        // i));
+        // }
     }
 }
--- src/leetcode/two_thousand_24/binary_search/find_kth_smallest_pair_distance/Solution.java
+++ src/leetcode/two_thousand_24/binary_search/find_kth_smallest_pair_distance/Solution.java
@@ -3,7 +3,7 @@ package leetcode.two_thousand_24.binary_search.find_kth_smallest_pair_distance;
 import java.util.Arrays;
 
 public class Solution {
-    private int getCount(int[] nums, int mi) {
+    public int getCount(int[] nums, int mi) {
         int r = 1;
         int count = 0;
         while (r < nums.length) {
@@ -24,8 +24,13 @@ public class Solution {
             int mi = lo + (hi - lo) / 2;
             int count = getCount(nums, mi);
             if (count < k) {
+                // we are pretty certain that the result is not found from lo to mi
+                // that is why we set the lo to mi + 1
+                // because result only can be be found between mi + 1 to hi
                 lo = mi + 1;
             } else {
+                // because count might equal to k but it doesn't ensure that mi is the optimal
+                // that is why we further check from lo to hi in next iteration
                 hi = mi;
             }
         }
