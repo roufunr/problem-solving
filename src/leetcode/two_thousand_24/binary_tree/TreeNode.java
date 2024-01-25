6d70a03e53d00b4f98456991d899c46ec6fdf54f
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-01-25 09:42:38 +0000

    Added tree node class

---
+++ src/leetcode/two_thousand_24/binary_tree/TreeNode.java
@@ -0,0 +1,20 @@
+package leetcode.two_thousand_24.binary_tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
