6d8713f2af4ce44308f813996407c6552151d16c
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-01-28 01:51:11 +0000

    Solved postorder iteratively

--- src/leetcode/two_thousand_24/binary_tree/inorder/Runner.java
+++ src/leetcode/two_thousand_24/binary_tree/inorder/Runner.java
@@ -1,12 +1,13 @@
 package leetcode.two_thousand_24.binary_tree.inorder;
 
 import leetcode.two_thousand_24.binary_tree.tree.TreeNode;
+import leetcode.two_thousand_24.binary_tree.postorder.Solution;
 
 public class Runner {
     public static void main(String[] args) {
         TreeNode three = new TreeNode(3, null, null);
         TreeNode two = new TreeNode(2, three, null);
         TreeNode one = new TreeNode(1, null, two);
-        new Solution().inorderTraversal(one);
+        new Solution().postorderTraversal(one);
     }
 }
--- src/leetcode/two_thousand_24/binary_tree/postorder/Solution.java
+++ src/leetcode/two_thousand_24/binary_tree/postorder/Solution.java
@@ -1,7 +1,11 @@
 package leetcode.two_thousand_24.binary_tree.postorder;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.List;
+import java.util.Stack;
+
 import leetcode.two_thousand_24.binary_tree.tree.*;
 
 public class Solution {
@@ -14,9 +18,33 @@ public class Solution {
         list.add(node.val);
     }
 
+    private void walk_v1(TreeNode root, List<Integer> list) { // iterative approach
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        Set<TreeNode> explored = new HashSet<>();
+        stack.push(root);
+        TreeNode current = root.left;
+        while (!stack.isEmpty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            TreeNode top = stack.peek();
+            if (top.right != null && !explored.contains(top.right)) {
+                current = top.right;
+            } else {
+                list.add(top.val);
+                explored.add(top);
+                stack.pop();
+            }
+        }
+    }
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        walk(root, ans);
+        walk_v1(root, ans);
         return ans;
     }
 }
