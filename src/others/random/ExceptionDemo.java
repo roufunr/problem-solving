3502545792331dd937a0d2685b5dbe74ddcc94c6
roufunr <rouf@nevada.unr.edu>
2024-03-17 14:01:26 -0700

    exception demo

---
+++ src/others/random/ExceptionDemo.java
@@ -0,0 +1,25 @@
+package others.random;
+
+import java.util.Scanner;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        int validCount = 0;
+        while(validCount < 1) {
+            try {
+                a = scanner.nextInt();
+                validCount++;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("Invalid Input");
+            }
+        }
+        b = scanner.nextInt();
+
+        System.out.println("Summation " + (a +b));
+    }
+}
