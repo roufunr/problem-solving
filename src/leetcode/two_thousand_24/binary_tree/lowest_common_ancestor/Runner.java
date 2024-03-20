50cf1aeb303c84645470e5eac54d29a90072de05
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-03-20 19:23:10 +0000

    solved but memory inefficient

---
+++ src/leetcode/two_thousand_24/binary_tree/lowest_common_ancestor/Runner.java
@@ -0,0 +1,38 @@
+package leetcode.two_thousand_24.binary_tree.lowest_common_ancestor;
+
+import leetcode.two_thousand_24.binary_tree.tree.TreeNode;
+
+public class Runner {
+    public static void main(String[] args) {
+        // TreeNode node3 = new TreeNode(3);
+        // TreeNode node5 = new TreeNode(5);
+        // TreeNode node1 = new TreeNode(1);
+        // TreeNode node6 = new TreeNode(6);
+        // TreeNode node2 = new TreeNode(2);
+        // TreeNode node0 = new TreeNode(0);
+        // TreeNode node8 = new TreeNode(8);
+        // TreeNode node7 = new TreeNode(7);
+        // TreeNode node4 = new TreeNode(4);
+
+        // node2.left = node7;
+        // node2.right = node4;
+
+        // node5.left = node6;
+        // node5.right = node2;
+
+        // node1.left = node0;
+        // node1.right = node8;
+
+        // node3.left = node5;
+        // node3.right = node1;
+
+        // new Solution().lowestCommonAncestor(node3, node5, node1);
+
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+
+        TreeNode node1 = new TreeNode(1, node2, node3);
+        new Solution().lowestCommonAncestor(node1, node2, node3);
+
+    }
+}
--- src/leetcode/two_thousand_24/binary_tree/lowest_common_ancestor/Solution.java
+++ src/leetcode/two_thousand_24/binary_tree/lowest_common_ancestor/Solution.java
@@ -1,32 +1,54 @@
 package leetcode.two_thousand_24.binary_tree.lowest_common_ancestor;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
+import leetcode.two_thousand_23.queue_stack.clone_graph.Node;
 import leetcode.two_thousand_24.binary_tree.tree.TreeNode;
 
 public class Solution {
     public List<TreeNode> path(TreeNode root, TreeNode node) {
-        List<TreeNode> p = new ArrayList<>();
-        TreeNode itrNode = root;
-        while (itrNode != null) {
-            TreeNode savedNode = itrNode;
-            p.add(savedNode);
-            if (itrNode.val == node.val) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        while (!queue.isEmpty()) {
+            TreeNode top = queue.poll();
+            if (top.val == node.val) {
                 break;
-            } else if (itrNode.val > node.val) {
-                itrNode = itrNode.right;
-            } else {
-                itrNode = itrNode.left;
+            }
+            if (top.left != null) {
+                parent.put(top.left, top);
+                queue.offer(top.left);
+            }
+            if (top.right != null) {
+                parent.put(top.right, top);
+                queue.offer(top.right);
             }
         }
+        List<TreeNode> p = new ArrayList<>();
+        TreeNode itrNode = node;
+        while (true) {
+            p.add(itrNode);
+            if (parent.containsKey(itrNode)) {
+                itrNode = parent.get(itrNode);
+            } else {
+                break;
+            }
+        }
+
+        Collections.reverse(p);
         return p;
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         List<TreeNode> p1 = path(root, p);
         List<TreeNode> p2 = path(root, q);
-        List<TreeNode> smallList = p1.size() < p2.size() ? p1 : p2;
+        List<TreeNode> smallList = p1.size() <= p2.size() ? p1 : p2;
         List<TreeNode> largeList = p1.size() > p2.size() ? p1 : p2;
         for (int i = smallList.size() - 1; i >= 0; i--) {
             if (smallList.get(i) == largeList.get(i)) {
