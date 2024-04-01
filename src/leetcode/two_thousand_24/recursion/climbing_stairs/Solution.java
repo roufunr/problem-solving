d200f0c768895f055d496f9f5d0a95b690c0afd6
roufunr <rouf@nevada.unr.edu>
2024-03-31 23:04:01 -0700

    climbing stairs done

--- src/leetcode/two_thousand_24/recursion/climbing_stairs/Solution.java
+++ src/leetcode/two_thousand_24/recursion/climbing_stairs/Solution.java
@@ -1,22 +1,23 @@
-package leetcode.two_thousand_24.recursion.fibonacci;
+package leetcode.two_thousand_24.recursion.climbing_stairs;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+
     private Map<Integer, Integer> map;
     
     public Solution() {
         map = new HashMap<>();
     }
-    public int fib(int n) {
+    public int climbStairs(int n) {
         if(n == 0 || n == 1) {
             return n;
         }
         if(map.containsKey(n)) {
             return map.get(n);
         }
-        int fib_n = fib(n-1) + fib(n-2);
+        int fib_n = climbStairs(n-1) + climbStairs(n-2);
         map.put(n, fib_n);
         return fib_n;
     }
