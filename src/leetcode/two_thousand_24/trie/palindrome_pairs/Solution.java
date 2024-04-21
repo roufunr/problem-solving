eea25c923020d6927944e2838c71f0a6a982b834
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-04-21 01:38:40 +0000

    started palindrome pair problem

---
+++ src/leetcode/two_thousand_24/trie/palindrome_pairs/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.two_thousand_24.trie.palindrome_pairs;
+
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+
+        return null;
+    }
+}
