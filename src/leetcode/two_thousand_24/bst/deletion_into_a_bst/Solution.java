1e622f3c1eeac7913c3e0f8547b61fa50f18d2be
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-04-28 04:43:38 +0000

    solved deletion in bst

---
+++ src/leetcode/two_thousand_24/bst/deletion_into_a_bst/Solution.java
@@ -0,0 +1,36 @@
+package leetcode.two_thousand_24.bst.deletion_into_a_bst;
+
+import leetcode.two_thousand_24.bst.TreeNode;
+
+public class Solution {
+    private int findMin(TreeNode node) {
+        if(node == null) {
+            return Integer.MAX_VALUE;
+        }
+
+        return Math.min(node.val, findMin(node.left));
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                int rightMin = findMin(root.right);
+                root.val = rightMin;
+                return deleteNode(root.right, rightMin);
+            }
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+
+        return root;
+    }
+}
