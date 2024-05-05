4410bd2737f169a33a552bfdd90925044fe082c3
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-05-05 04:07:33 +0000

    solved dupllicate III problem

--- src/leetcode/two_thousand_24/bst/contains_duplicate_III/Solution.java
+++ src/leetcode/two_thousand_24/bst/contains_duplicate_III/Solution.java
@@ -1,18 +1,9 @@
 package leetcode.two_thousand_24.bst.contains_duplicate_III;
 
 import java.util.Comparator;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
-
-class Pair {
-    public int val;
-    public int idx;
-
-    public Pair(int val, int idx) {
-        this.val = val;
-        this.idx = idx;
-    }
-}
 
 public class Solution {
 
@@ -35,24 +26,25 @@ public class Solution {
     }
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-        Comparator<Pair> valComparator = new Comparator<Pair>() {
-            @Override
-            public int compare(Pair p1, Pair p2) {
-                return Integer.compare(p1.val, p2.val);
-            }
-        };
-        PriorityQueue<Pair> pq = new PriorityQueue<>(valComparator);
+        Map<Integer, Integer> bucketMap = new HashMap<>();
+        int windowSize = indexDiff + 1;
         for (int i = 0; i < nums.length; i++) {
-            if (pq.size() < (indexDiff + 1)) {
-                pq.add(new Pair(nums[i], i));
-            }
-            if (pq.peek().idx != i && Math.abs(nums[i] - pq.peek().val) <= valueDiff) {
+            int bucket = Math.floorDiv(nums[i], (valueDiff + 1));
+            if (bucketMap.containsKey(bucket)) {
                 return true;
             }
-            if (pq.size() == (indexDiff + 1)) {
-                pq.poll();
+            if (bucketMap.containsKey(bucket - 1) && Math.abs(bucketMap.get(bucket - 1) - nums[i]) <= valueDiff) {
+                return true;
             }
+            if (bucketMap.containsKey(bucket + 1) && Math.abs(bucketMap.get(bucket + 1) - nums[i]) <= valueDiff) {
+                return true;
+            }
+            bucketMap.put(bucket, nums[i]);
 
+            if (bucketMap.size() > indexDiff) {
+                bucketMap.remove(Math.floorDiv(nums[i - indexDiff], valueDiff + 1));
+            }
+            
         }
         return false;
     }
