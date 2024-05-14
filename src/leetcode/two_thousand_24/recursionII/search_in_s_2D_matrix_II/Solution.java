774dd7e61bfa41b738c440c561cdf8e8f396f53f
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-05-14 21:01:26 +0000

    Solved most efficient way

---
+++ src/leetcode/two_thousand_24/recursionII/search_in_s_2D_matrix_II/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.two_thousand_24.recursionII.search_in_s_2D_matrix_II;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
