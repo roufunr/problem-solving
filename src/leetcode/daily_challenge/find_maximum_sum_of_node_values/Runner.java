78f08581518c4d155f33e315ae3c096c97ddf7bf
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-05-19 10:42:58 +0000

    partially solved

--- src/leetcode/daily_challenge/find_maximum_sum_of_node_values/Runner.java
+++ src/leetcode/daily_challenge/find_maximum_sum_of_node_values/Runner.java
@@ -2,10 +2,10 @@ package leetcode.daily_challenge.find_maximum_sum_of_node_values;
 
 public class Runner {
     public static void main(String[] args) {
-        int[] nums = {24,78,1,97,44};
-        int k = 6;
-        int[][] edges = {{0,2},{1,2},{4,2},{3,4}};
-        new Solution().maximumValueSum(nums, 6, edges);
+        int[] nums = { 67, 13, 79, 13, 75, 11, 0, 41, 94 };
+        int k = 7;
+        int[][] edges = { { 0, 1 }, { 3, 7 }, { 4, 7 }, { 6, 5 }, { 6, 0 }, { 0, 2 }, { 7, 2 }, { 7, 8 } };
+        new Solution().maximumValueSum(nums, k, edges);
 
     }
 }
--- src/leetcode/daily_challenge/find_maximum_sum_of_node_values/Solution.java
+++ src/leetcode/daily_challenge/find_maximum_sum_of_node_values/Solution.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
         List<Integer> positiveImpactIdx = new ArrayList<>();
+        List<Integer> negativeImpactIdx = new ArrayList<>();
         long maxSum = 0;
         for (int i = 0; i < nums.length; i++) {
             if ((nums[i] ^ k) >= nums[i]) {
@@ -13,6 +14,7 @@ public class Solution {
                 maxSum += (nums[i] ^ k);
             } else {
                 maxSum += nums[i];
+                negativeImpactIdx.add(i);
             }
         }
         if (positiveImpactIdx.size() % 2 == 1) {
@@ -21,10 +23,23 @@ public class Solution {
             for (int i = 0; i < positiveImpactIdx.size(); i++) {
                 if ((nums[positiveImpactIdx.get(i)] ^ k) < minImpact) {
                     minIdx = positiveImpactIdx.get(i);
-                    minImpact = nums[minIdx] ^ k;
+                    minImpact = (nums[minIdx] ^ k) - nums[minIdx];
                 }
             }
-            maxSum = maxSum + nums[minIdx] - minImpact;
+            // maxSum = maxSum + nums[minIdx] - minImpact;
+            int negMinIdx = -1;
+            int negMinImpact = Integer.MAX_VALUE;
+            for (int i = 0; i < negativeImpactIdx.size(); i++) {
+                int absVal = Math.abs((nums[negativeImpactIdx.get(i)] ^ k) - nums[negativeImpactIdx.get(i)]);
+                if (absVal < negMinImpact) {
+                    negMinIdx = negativeImpactIdx.get(i);
+                    negMinImpact = absVal;
+                }
+            }
+
+            if (minImpact - negMinImpact >= 0) {
+                maxSum -= negMinImpact;
+            }
         }
         return maxSum;
     }
