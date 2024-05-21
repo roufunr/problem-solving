4208e8a0a1733ec458915b496841700da82e07ec
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-05-21 09:31:04 +0000

    solved subset

---
+++ src/leetcode/daily_challenge/subsets/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.daily_challenge.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    // 13
+    // 13 -> 6 1
+    // 6 -> 3 0
+    // 3 -> 1 1
+    // 1 -> 0 1
+    private List<Integer> getSubset(int[] nums, int serial) {
+        List<Integer> subset = new ArrayList<>();
+        int reminder = 0;
+        int dividend = serial;
+        int i = 0;
+        while (dividend != 0) {
+            reminder = dividend % 2;
+            dividend = dividend / 2;
+            if (reminder == 1) {
+                subset.add(nums[i]);
+            }
+            i++;
+        }
+        return subset;
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int totalSubsets = (int) Math.pow(2, nums.length);
+        if (totalSubsets == 1) {
+            return ans;
+        }
+        for (int i = 0; i < totalSubsets; i++) {
+            ans.add(getSubset(nums, i));
+        }
+        return ans;
+    }
+}
