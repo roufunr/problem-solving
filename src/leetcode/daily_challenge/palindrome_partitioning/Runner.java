52536f6a5163a14408900cd414de8c9de7470462
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-05-23 20:00:48 +0000

    palindrome partitioning done

---
+++ src/leetcode/daily_challenge/palindrome_partitioning/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.palindrome_partitioning;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(new Solution().partition("aab"));
+    }
+}
