f9c761595eb5a2347621b31b8136061c530e02a2
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-05-27 22:32:32 +0000

    solved special array problem

---
+++ src/leetcode/daily_challenge/special_array/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.daily_challenge.special_array;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int specialArray(int[] nums) {
+        int[] counts = new int[nums.length + 1];
+
+        for (int j = 0; j <= nums.length; j++) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= j) {
+                    counts[j]++;
+                }
+            }
+        }
+
+        for (int i = 0; i <= nums.length; i++) {
+            if (counts[i] == i) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
