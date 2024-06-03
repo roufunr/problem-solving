f121f258d1fb2a0a1844ad922e4ad7a5f47bc85b
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-03 18:23:54 +0000

    daily challenge append character done

---
+++ src/leetcode/daily_challenge/count_triplets/Runner.java
@@ -0,0 +1,9 @@
+package leetcode.daily_challenge.count_triplets;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 1, 1, 1 };
+        new SolutionV3().countTriplet(arr);
+    }
+
+}
