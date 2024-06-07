f139c36c134ebd592c17cdc9b23648371d2e028e
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-07 05:40:47 +0000

    min heap based another solution added

--- src/leetcode/daily_challenge/group_of_hands/Runner.java
+++ src/leetcode/daily_challenge/group_of_hands/Runner.java
@@ -2,7 +2,7 @@ package leetcode.daily_challenge.group_of_hands;
 
 public class Runner {
     public static void main(String[] args) {
-        int[] hand = { 1, 2, 3, 6, 2, 3, 4, 7, 8 };
+        int[] hand = {1,2,3,6,2,4,7,8,5 };
         int groupSize = 3;
         new Solution().isNStraightHand(hand, groupSize);
     }
--- src/leetcode/daily_challenge/group_of_hands/Solution.java
+++ src/leetcode/daily_challenge/group_of_hands/Solution.java
@@ -3,6 +3,7 @@ package leetcode.daily_challenge.group_of_hands;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
@@ -19,6 +20,9 @@ public class Solution {
         while(i < keys.length - groupSize + 1) {
             int cursor = i;
             for(int j = cursor; j < cursor + groupSize; j++) {
+                if(map.get(keys[j]) == 0) {
+                    return false;
+                }
                 if(j == cursor) {
                     map.put(keys[j], map.get(keys[j]) - 1);
                 } else {
@@ -41,5 +45,33 @@ public class Solution {
         }
         return true;
     }
-}
 
+    // minHeap based Solution
+    public boolean isNStraightHandV1(int[] hand, int groupSize) {
+		if(hand.length % groupSize != 0) {
+			return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n : hand) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(Integer key : map.keySet()) {
+            pq.add(key);
+        }
+        while(!pq.isEmpty()) {
+            int minValue = pq.peek();
+            for(int i = minValue; i < minValue + groupSize; i++) {
+                if(map.containsKey(i) && map.get(i) > 0) {
+                    map.put(i, map.get(i) - 1);
+                    if(map.get(i) == 0) {
+                        pq.poll();
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
