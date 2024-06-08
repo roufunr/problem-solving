8011ce6ceecef07a89ba18fa40b5d70208a24db8
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-08 02:29:58 +0000

    continuous subarray sum done

--- src/leetcode/daily_challenge/continuous_subarray/Solution.java
+++ src/leetcode/daily_challenge/continuous_subarray/Solution.java
@@ -1,5 +1,8 @@
 package leetcode.daily_challenge.continuous_subarray;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     // got TLE
     public boolean checkSubarraySumV0(int[] nums, int k) {
@@ -21,6 +24,30 @@ public class Solution {
         }
         if (nums.length > 1 && prefixSum[2] % k == 0) {
             return true;
+        }
+        return false;
+    }
+
+
+    // order of N solution 
+    // accepted
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int[] prefixSum = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            prefixSum[i] = sum;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            int remainder = prefixSum[i] % k;
+            if (map.containsKey(remainder)) {
+                if (i - map.get(remainder) > 1)
+                    return true;
+            } else {
+                map.put(remainder, i);
+            }
         }
         return false;
     }
