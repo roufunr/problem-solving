0d18b4bf55c9f21a4ac9aaffc406ffe4edb807f4
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-10 05:40:02 +0000

    daily challenge height checker done

---
+++ src/leetcode/daily_challenge/height_checkers/Runner.java
@@ -0,0 +1,8 @@
+package leetcode.daily_challenge.height_checkers;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] heights = { 1, 1, 4, 2, 1, 3 };
+        new Solution().heightChecker(heights);
+    }
+}
