66e313ea65a9590e472da56f14ce0de76062dc38
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-13 04:54:03 +0000

    min moves to seat

---
+++ src/leetcode/daily_challenge/min_moves_to_seat/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.daily_challenge.min_moves_to_seat;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int count = 0;
+        for (int i = 0; i < seats.length; i++) {
+            count += Math.abs(seats[i] - students[i]);
+        }
+        return count;
+    }
+}
