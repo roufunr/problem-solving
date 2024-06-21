ec3f61cf3b07020be2a85920f0ccea70886bcab1
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-21 17:49:09 +0000

    solved grumpy in N

---
+++ src/leetcode/daily_challenge/grumpy_bookstore/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.daily_challenge.grumpy_bookstore;
+
+public class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int maxUtil = 0;
+        for (int i = 0; i < minutes; i++) {
+            if (grumpy[i] == 1) {
+                maxUtil += customers[i];
+            }
+        }
+        int idx = 0;
+        int newUtil = maxUtil;
+        for (int i = 1; i < customers.length - minutes + 1; i++) {
+            if (grumpy[i - 1] == 1) {
+                newUtil -= customers[i - 1];
+            }
+            if (grumpy[i + minutes - 1] == 1) {
+                newUtil += customers[i + minutes - 1];
+            }
+            // System.out.println(newUtil);
+            maxUtil = maxUtil < newUtil ? newUtil : maxUtil;
+            idx = maxUtil < newUtil ? i : idx;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < customers.length; i++) {
+            ans += grumpy[i] == 0 ? customers[i] : 0;
+        }
+        ans += maxUtil;
+        return ans;
+    }
+}
