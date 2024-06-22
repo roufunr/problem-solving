3f54aaa8843337eed0d134d0058dc493a14bdc50
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-22 05:07:14 +0000

    solved nice sub array

---
+++ src/leetcode/daily_challenge/count_nice_subarray/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.daily_challenge.count_nice_subarray;
+
+import java.util.*;
+
+public class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        Map<Integer, Integer> set = new HashMap<>();
+        int count = 0;
+        int ans = 0;
+        set.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                count++;
+            }
+            set.put(count, set.getOrDefault(count, 0) + 1);
+            if (set.containsKey(count - k)) {
+                ans += set.get(count - k);
+            }
+        }
+        return ans;
+    }
+}
