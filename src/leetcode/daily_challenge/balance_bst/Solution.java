3089ab9afb89b0896a2cf099be768d8261e47dfd
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-26 19:05:32 +0000

    balance binary tree code done

---
+++ src/leetcode/daily_challenge/balance_bst/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.daily_challenge.balance_bst;
+
+import java.util.*;
+import leetcode.two_thousand_24.bst.TreeNode;
+
+public class Solution {
+    private List<Integer> elements;
+
+    private void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        elements.add(node.val);
+        inorder(node.right);
+    }
+
+    private TreeNode constructTree(List<Integer> elements, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(elements.get(mid));
+        node.left = constructTree(elements, start, mid - 1);
+        node.right = constructTree(elements, mid + 1, end);
+        return node;
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+        elements = new ArrayList<>();
+        inorder(root);
+        Collections.sort(elements);
+        return constructTree(elements, 0, elements.size() - 1);
+    }
+}
