c45274c136d900daaa492808c563fe3ce3c0b257
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-06-30 21:31:04 +0000

    refactored

--- src/leetcode/extras/redundant_connection/Solution.java
+++ src/leetcode/extras/redundant_connection/Solution.java
@@ -1,10 +1,11 @@
 package leetcode.extras.redundant_connection;
+
 public class Solution {
-    
+
     private int find(int v, int[] parent) {
         int p = parent[v];
-        while(p != parent[p]) {
-            parent[p] = parent[parent[p]];
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]]; // path shortened
             p = parent[p];
         }
         return p;
@@ -13,11 +14,11 @@ public class Solution {
     private boolean union(int u, int v, int[] parent, int[] rank) {
         int fu = find(u, parent);
         int fv = find(v, parent);
-        if(fu == fv) {
+        if (fu == fv) {
             return false;
-        } 
+        }
 
-        if(rank[fu] > rank[fv]) {
+        if (rank[fu] > rank[fv]) {
             parent[fv] = fu;
             rank[fu] += rank[fv];
         } else {
@@ -26,20 +27,20 @@ public class Solution {
         }
         return true;
     }
-    
+
     public int[] findRedundantConnection(int[][] edges) {
         int[] parent = new int[edges.length + 1];
-        int[] rank = new int[edges.length + 1]; 
-        for(int i = 1; i < parent.length; i++) {
+        int[] rank = new int[edges.length + 1];
+        for (int i = 1; i < parent.length; i++) {
             parent[i] = i;
             rank[i] = 1;
         }
 
-        for(int[] edge : edges) {
-            if(!union(edge[0], edge[1], parent, rank)) {
+        for (int[] edge : edges) {
+            if (!union(edge[0], edge[1], parent, rank)) {
                 return edge;
             }
         }
-        return edges[edges.length - 1]; 
+        return edges[edges.length - 1];
     }
 }
