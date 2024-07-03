ad59c74c4b569a5be56139b2c5e96ce2c62131d4
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-03 20:30:53 +0000

    minimum difference  both solution

---
+++ src/leetcode/daily_challenge/minimum_difference/Runner.java
@@ -0,0 +1,9 @@
+package leetcode.daily_challenge.minimum_difference;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] nums = { 0, 1, 1, 4, 6, 6, 6 };
+        new SolutionII().minDifference(nums);
+
+    }
+}
---
+++ src/leetcode/daily_challenge/minimum_difference/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.daily_challenge.minimum_difference;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int minDifference(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length <= 4) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0, j = nums.length - 4; i < 4; i++) {
+            ans = Math.min(ans, nums[j] - nums[i]);
+            j++;
+        }
+        return ans;
+    }
+}
