8bd03d5989325782fe0fac31e39f6bb06bee6a4d
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-09 01:45:49 +0000

    average wait time done

--- src/leetcode/daily_challenge/find_winner/Runner.java
+++ src/leetcode/daily_challenge/find_winner/Runner.java
@@ -2,6 +2,6 @@ package leetcode.daily_challenge.find_winner;
 
 public class Runner {
     public static void main(String[] args) {
-        new Solution().findTheWinner(5, 2);
+        new SolutionIII().findTheWinner(5, 2);
     }
 }
