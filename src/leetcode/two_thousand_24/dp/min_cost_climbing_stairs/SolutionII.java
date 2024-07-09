77ee7deaaa6b2f165953b7e8af804094f3c21411
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-09 06:32:52 +0000

    Solved the iterative solution for min cost to climb stairs

---
+++ src/leetcode/two_thousand_24/dp/min_cost_climbing_stairs/SolutionII.java
@@ -0,0 +1,13 @@
+package leetcode.two_thousand_24.dp.min_cost_climbing_stairs;
+
+import java.util.HashMap;
+
+public class SolutionII { // iterative solution // faster
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2]);
+        }
+        return dp[dp.length - 1];
+    }
+}
