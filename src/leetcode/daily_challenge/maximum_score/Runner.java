37b08f78ecc95130401c033a5cacc2a953cbc3cc
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-12 23:15:51 +0000

    solved the high priority and low priority str deletion

---
+++ src/leetcode/daily_challenge/maximum_score/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.maximum_score;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().maximumGain("cdbcbbaaabab", 4, 5);
+    }
+}
---
+++ src/leetcode/daily_challenge/reverse_string/SolutionII.java
@@ -0,0 +1,26 @@
+package leetcode.daily_challenge.reverse_string;
+
+import java.util.Stack;
+
+public class SolutionII {
+    public String reverseParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(sb.length());
+            } else if (c == ')') {
+                int startIdx = stack.pop();
+                int endIdx = sb.length() - 1;
+                for (int i = 0; i < (endIdx - startIdx + 1) / 2; i++) {
+                    char temp = sb.charAt(startIdx + i);
+                    sb.setCharAt(startIdx + i, sb.charAt(endIdx - i));
+                    sb.setCharAt(endIdx - i, temp);
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
