adb04b5489ebe25e7e74d9112e7a4db1ad51f6b8
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-14 12:06:34 +0000

    solved the number of atoms problem

---
+++ src/leetcode/daily_challenge/number_of_atoms/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.number_of_atoms;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().countOfAtoms("K4(ON(SO3)2)2");
+    }
+}
