bcebfc941346a81eaa734d125d2ba5ab3a9688f4
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-20 20:51:58 +0000

    Solved matrix row  and column sum

--- src/leetcode/daily_challenge/find_valid_matrix_from_rowsum_colsum/Solution.java
+++ src/leetcode/daily_challenge/find_valid_matrix_from_rowsum_colsum/Solution.java
@@ -2,33 +2,37 @@ package leetcode.daily_challenge.find_valid_matrix_from_rowsum_colsum;
 
 public class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        int[][] ans = new int[rowSum.length][colSum.length];
-        int total = 0;
+        long[][] ans = new long[rowSum.length][colSum.length];
+        long total = 0;
         for(int i = 0; i < rowSum.length; i++) {
             ans[i][0] = rowSum[i];
             total+= rowSum[i];
         }
+        System.out.println(total);
         for(int j = 0; j < colSum.length - 1; j++) {
-            int compensate = total - colSum[j];
+            long compensate = total - colSum[j];
             total -= colSum[j];
             for(int i = 0; i < rowSum.length; i++) {
                 if(compensate <= 0) {
                     break;
                 }
                 if(compensate > ans[i][j]) {
-                    ans[i][j] = 0;
                     ans[i][j + 1] = ans[i][j];
-                    compensate -= ans[i][j];
+                    ans[i][j] = 0;
+                    compensate -= ans[i][j + 1];
                 } else {
                     ans[i][j + 1] = compensate;
                     ans[i][j] -= compensate;
                     compensate = 0;
                 }
             }
-            if(total == 0) {
-                break;
+        }
+        int[][] result = new int[ans.length][ans[0].length];
+        for(int i = 0; i < result.length; i++) {
+            for(int j = 0; j < result[0].length; j++) {
+                result[i][j] = (int) ans[i][j];
             }
         }
-        return ans;
+        return result;
     }
 }
