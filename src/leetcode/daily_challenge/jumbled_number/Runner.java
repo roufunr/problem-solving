8f8e60c4bce7077da891ddfeec3c69db9e1c4606
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-07-24 05:01:52 +0000

    Solved the jumbled number with lambda implementation

---
+++ src/leetcode/daily_challenge/jumbled_number/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.jumbled_number;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().sortJumbled(new int[]{8,9,4,0,2,1,3,5,7,6}, new int[]{991,338,38});
+    }
+}
