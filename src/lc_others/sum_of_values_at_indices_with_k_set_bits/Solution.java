86da476c3be8446f72b58589bb804e1336d12151
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-08-10 02:39:45 +0000

    Solved sum-of-values-at-indices-with-k-set-bits

---
+++ src/lc_others/sum_of_values_at_indices_with_k_set_bits/Solution.java
@@ -0,0 +1,24 @@
+package lc_others.sum_of_values_at_indices_with_k_set_bits;
+
+public class Solution {
+    private int getSetBitsCount(int n) {
+        int count = 0;
+        int N = n;
+        while (N > 0) {
+            count += (N % 2);
+            N = N / 2;
+        }
+        // System.out.println(n + " -> " + count);
+        return count;
+    }
+
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (getSetBitsCount(i) == k) {
+                sum += nums.get(i);
+            }
+        }
+        return sum;
+    }
+}
