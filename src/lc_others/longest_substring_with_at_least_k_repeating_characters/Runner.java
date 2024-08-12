51de3fcd11307149906049705ec55ea2ee282484
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-08-12 04:46:37 +0000

    Solved longest_substring_with_at_least_k_repeating_characters

---
+++ src/lc_others/longest_substring_with_at_least_k_repeating_characters/Runner.java
@@ -0,0 +1,7 @@
+package lc_others.longest_substring_with_at_least_k_repeating_characters;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().longestSubstring("bchhbbdefghiaaacb", 3);
+    }
+}
---
+++ src/lc_others/longest_substring_with_at_least_k_repeating_characters/Solution.java
@@ -0,0 +1,23 @@
+package lc_others.longest_substring_with_at_least_k_repeating_characters;
+
+public class Solution {
+    public int longestSubstring(String s, int k) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int[] count = new int[26];
+            for (int j = i; j < s.length(); j++) {
+                count[(int) (s.charAt(j) - 'a')]++;
+                int lowest = Integer.MAX_VALUE;
+                for (int l = 0; l < 26; l++) {
+                    if (count[l] != 0) {
+                        lowest = Math.min(lowest, count[l]);
+                    }
+                }
+                if(lowest >= k) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
