8d6ed176a8a96f3795d76fb26cf489d4d5fb1d9e
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-08-12 03:42:35 +0000

    Solved the build array of permuttation

---
+++ src/lc_others/build_the_array_of_permuttation/Solution.java
@@ -0,0 +1,11 @@
+package lc_others.build_the_array_of_permuttation;
+
+public class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
