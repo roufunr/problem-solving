c738b12a587126888c90039513c149b0a99f3589
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-08-19 05:26:25 +0000

    Solved the two keys keyboard

---
+++ src/leetcode/daily_challenge/august_24/two_keys_keyboard/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.daily_challenge.august_24.two_keys_keyboard;
+
+public class Solution {
+    private boolean isPrime(int x) {
+        if (x == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int minSteps(int n) {
+        int ans = 0;
+        int i = 2;
+        while (n > 1) {
+            if (n % i == 0) {
+                if (isPrime(i)) {
+                    ans += i;
+                } else {
+                    ans += minSteps(i);
+                }
+                n = n / i;
+            } else {
+                i++;
+            }
+        }
+        return ans;
+    }
+}
