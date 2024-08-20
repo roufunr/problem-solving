9666851e1d7eb4346b836495fec68e25b5acded2
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-08-20 20:06:48 +0000

    Solved stone_game_II using top down approach

---
+++ src/leetcode/daily_challenge/august_24/stone_game_II/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.august_24.stone_game_II;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().stoneGameII(new int[] { 2, 7, 9, 4, 4 });
+    }
+}
---
+++ src/leetcode/daily_challenge/august_24/stone_game_II/Solution.java
@@ -0,0 +1,36 @@
+package leetcode.daily_challenge.august_24.stone_game_II;
+
+public class Solution {
+    private int[][][] cache;
+
+    private int dp(int[] piles, int M, int idx, int turn) {
+        if (piles.length <= idx) {
+            return 0;
+        }
+        if (cache[idx][M - 1][turn] != 0) {
+            return cache[idx][M - 1][turn];
+        }
+        if (turn == 0) {
+            int maxScore = Integer.MIN_VALUE;
+            int totalScore = 0;
+            for (int i = 1; i <= (2 * M) && (i + idx <= piles.length); i++) {
+                totalScore += piles[idx + i - 1];
+                maxScore = Math.max(maxScore, totalScore + dp(piles, Math.max(M, i), idx + i, 1));
+            }
+            cache[idx][M - 1][turn] = maxScore;
+            return maxScore;
+        } else {
+            int minScore = Integer.MAX_VALUE;
+            for (int i = 1; i <= (2 * M) && (i + idx <= piles.length); i++) {
+                minScore = Math.min(minScore, dp(piles, Math.max(M, i), idx + i, 0));
+            }
+            cache[idx][M - 1][turn] = minScore;
+            return minScore;
+        }
+    }
+
+    public int stoneGameII(int[] piles) {
+        cache = new int[piles.length][piles.length][2];
+        return dp(piles, 1, 0, 0);
+    }
+}
