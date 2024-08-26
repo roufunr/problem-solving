90ca7d5b79c9f172f499a6f1487da2926402919c
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-08-26 05:45:44 +0000

    SOlved n_ary_tree_postorder_traversal

---
+++ src/leetcode/daily_challenge/august_24/n_ary_tree_postoder_traversal/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.daily_challenge.august_24.n_ary_tree_postorder_traversal;
+
+import java.util.*;
+public class Solution {
+    private List<Integer> postOrderList;
+    private void traverse(Node node) {
+        if(node == null) {
+            return;
+        }
+        for(Node child : node.children) {
+            traverse(child);
+        }
+        postOrderList.add(node.val);
+    }
+    public List<Integer> postorder(Node root) {
+        postOrderList = new ArrayList<>();
+        traverse(root);
+        return postOrderList;
+    }
+}
