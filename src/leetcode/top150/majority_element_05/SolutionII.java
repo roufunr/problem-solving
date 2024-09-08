f1f47663fdbfb6957a78d42584b7bbdc12cbe4f0
roufunr <arouf@unr.edu>
2024-09-07 18:53:02 -0700

    Solved the majority element problem using bit manipulation

---
+++ src/leetcode/top150/majority_element_05/SolutionII.java
@@ -0,0 +1,22 @@
+package leetcode.top150.majority_element_05;
+
+
+// Solved the majority element using bit manipulation
+public class SolutionII {
+    public int majorityElement(int[] nums) {
+        int majority = 0;
+        for(int i = 0; i < 32; i++) {
+            int bit = 1 << i;
+            int bitCount = 0;
+            for(int num : nums) {
+                if((num & bit) != 0) {
+                    bitCount++;
+                }
+            }
+            if(bitCount > (nums.length / 2)) {
+                majority |= bit;
+            }
+        }
+        return majority;
+    }
+}
