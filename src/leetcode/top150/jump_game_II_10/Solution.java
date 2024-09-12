645dba07661a74ae4832a1f4d4f89c4805b458a5
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-09-12 23:55:25 +0000

    Solved the jump game 02 using bfs

---
+++ src/leetcode/top150/jump_game_II_10/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.top150.jump_game_II_10;
+
+import java.util.*;
+
+public class Solution {
+    private int bfs(List<List<Integer>> adj) {
+        boolean[] visited = new boolean[adj.size()];
+        int[] parent = new int[adj.size()];
+        Arrays.fill(parent, -1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited[0] = true;
+        parent[0] = 0;
+        while (!queue.isEmpty()) {
+            int top = queue.poll();
+            for (int nei : adj.get(top)) {
+                if (nei < visited.length && visited[nei] == false) {
+                    parent[nei] = top;
+                    visited[nei] = true;
+                    queue.offer(nei);
+                }
+            }
+        }
+        int pathLen = 0;
+        int itr = adj.size() - 1;
+        while (itr != 0) {
+            pathLen++;
+            itr = parent[itr];
+        }
+        return pathLen;
+    }
+
+    public int jump(int[] nums) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i]; j++) {
+                adj.get(i).add(i + j + 1);
+            }
+        }
+        return bfs(adj);
+    }
+}
