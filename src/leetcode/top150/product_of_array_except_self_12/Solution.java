b5d68235a1a95c5f5948a51bc869be495a630a27
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-09-13 22:30:03 +0000

    Solved product_of_array_except_self using bruteforce

--- src/leetcode/top150/product_of_array_except_self_12/Solution.java
+++ src/leetcode/top150/product_of_array_except_self_12/Solution.java
@@ -2,29 +2,31 @@ package leetcode.top150.product_of_array_except_self_12;
 
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int zeroCount = 0;
         int mult = 0;
-        boolean zeroExist = false;
-        for (int n : nums) {
-            if (n != 0) {
-                if (mult == 0) {
-                    mult = n;
+        for(int n : nums) {
+            if(n == 0) {
+               zeroCount++; 
+            } else {
+                if(mult == 0) {
+                    mult += n;
                 } else {
                     mult *= n;
                 }
-            } else
-                zeroExist = true;
+            }
         }
         int[] ans = new int[nums.length];
-        for (int i = 0; i < ans.length; i++) {
-            if (zeroExist) {
-                if (nums[i] != 0)
-                    ans[i] = 0;
-                else
+        if(zeroCount >= 2) {
+            return ans;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(zeroCount == 1) {
+                if(nums[i] == 0) {
                     ans[i] = mult;
+                }
             } else {
                 ans[i] = mult / nums[i];
             }
-
         }
         return ans;
     }
