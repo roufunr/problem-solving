86e7fa7e458f9d9fe48c942b3e61280418f0c3cf
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-09-18 21:57:38 +0000

    Solved largest number using modified sorting

---
+++ src/leetcode/daily_challenge/september_24/largest_number_18/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.daily_challenge.september_24.largest_number_18;
+
+import java.util.*;
+
+public class Solution {
+
+    public String largestNumber(int[] nums) {
+
+        String[] numStrings = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numStrings[i] = Integer.toString(nums[i]);
+        }
+
+        Arrays.sort(numStrings, (a, b) -> (b + a).compareTo(a + b));
+
+        if (numStrings[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder largestNum = new StringBuilder();
+        for (String numStr : numStrings) {
+            largestNum.append(numStr);
+        }
+
+        return largestNum.toString();
+    }
+}
