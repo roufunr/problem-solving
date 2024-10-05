ec0066d7a5e255e7703cd876daef4c0757ba52a7
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-10-05 20:52:52 +0000

    Solved permuttation on string

---
+++ src/leetcode/daily_challenge/october_24/permuttation_in_string_05/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.daily_challenge.october_24.permuttation_in_string_05;
+
+public class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1arr[s1.charAt(i) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] == s2arr[i])
+                count++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2arr[r]++;
+            if (s2arr[r] == s1arr[r]) {
+                count++;
+            } else if (s2arr[r] == s1arr[r] + 1) {
+                count--;
+            }
+            s2arr[l]--;
+            if (s2arr[l] == s1arr[l]) {
+                count++;
+            } else if (s2arr[l] == s1arr[l] - 1) {
+                count--;
+            }
+        }
+        return count == 26;
+    }
+}
