3a24fe3a79c8f99b463b9369a7e5935c2c56e9ee
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-11-07 03:19:15 +0000

    Solved largest_combination_with_bitwise_and_greater_than_zero using plain array

--- src/leetcode/daily_challenge/november_24/largest_combination_with_bitwise_and_greater_than_zero_07/Solution.java
+++ src/leetcode/daily_challenge/november_24/largest_combination_with_bitwise_and_greater_than_zero_07/Solution.java
@@ -3,7 +3,7 @@ package leetcode.daily_challenge.november_24.largest_combination_with_bitwise_an
 import java.util.*;
 
 public class Solution {
-    public int largestCombination(int[] candidates) {
+    public int largestCombination_v0(int[] candidates) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < 32; i++) {
             map.put(i, 0);
@@ -20,6 +20,24 @@ public class Solution {
         int max = 0;
         for (int i = 0; i < 32; i++) {
             max = Math.max(max, map.get(i));
+        }
+        return max;
+    }
+
+    public int largestCombination(int[] candidates) {
+        int[] count = new int[32];
+        for (int n : candidates) {
+            int x = n;
+            int ith = 0;
+            while (x > 0) {
+                count[ith] += (x % 2);
+                x = x / 2;
+                ith++;
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < 32; i++) {
+            max = Math.max(max, count[i]);
         }
         return max;
     }
