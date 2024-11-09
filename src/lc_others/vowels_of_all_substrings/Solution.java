07be648570aa6247a4e24e0b6459827d988b641b
roufunr <arouf@unr.edu>
2024-11-09 14:53:48 -0800

    Solved vowels_of_all_substrings

---
+++ src/lc_others/vowels_of_all_substrings/Solution.java
@@ -0,0 +1,16 @@
+package lc_others.vowels_of_all_substrings;
+
+public class Solution {
+    public long countVowels(String word) {
+        long totalLen = (long)word.length();
+        long substrCount = 0;
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        for(long i = 0; i < totalLen; i++) {
+            char c = word.charAt((int)i);
+            if(set.contains(c)) {
+                substrCount += (long)(totalLen - 1 + i * (totalLen - 1 - i) + 1);
+            }
+        }
+        return substrCount;
+    }
+}
