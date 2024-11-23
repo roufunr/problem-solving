c27422e21ff1d0ce677f52946ca0a68f135477f8
roufunr <arouf@unr.edu>
2024-11-23 15:22:27 -0800

    Solved rotated box problem using brute force

--- src/leetcode/daily_challenge/november_24/rotating_the_box_23/Solution.java
+++ src/leetcode/daily_challenge/november_24/rotating_the_box_23/Solution.java
@@ -1,6 +1,10 @@
 package leetcode.daily_challenge.november_24.rotating_the_box_23;
 
 public class Solution {
+    // initially set the empty space pointer and read pointer at the last of each row
+    // when read pointer encouters empty space it just keeps moving to left
+    // when read pointer encounters stone then it places the stone at empty space and then move left both the pointer
+    // when read pointer encounters obstacle then it places the empty space pointer one idx left to the obstacle.
     public char[][] rotateTheBox(char[][] box) {
         for(int i = 0; i < box.length; i++) {
             int l = box[0].length - 1;
