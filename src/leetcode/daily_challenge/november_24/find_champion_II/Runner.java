3c293bc0bad17c40e99065f662086826799c0d84
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-11-26 21:46:44 +0000

    Solved find_champion_II

---
+++ src/leetcode/daily_challenge/november_24/find_champion_II/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.november_24.find_champion_II;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().findChampion(3, new int[][] { { 0, 1 }, { 1, 2 } });
+    }
+}
---
+++ src/leetcode/daily_challenge/november_24/find_champion_II/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.daily_challenge.november_24.find_champion_II;
+
+public class Solution {
+    public int findChampion(int n, int[][] edges) {
+        int[][] graph = new int[n][2];
+
+        for(int[] edge : edges) {
+            graph[edge[0]][0] += 1;
+            graph[edge[1]][1] += 1;
+        }
+
+        int ans = -1;
+        int count = 0;
+        int i = 0;
+        for(int[] v : graph) {
+            if((v[0] > 0  && v[1] == 0) || (v[0] == 0 && v[1] == 0)) {
+                count++;
+                ans = i;
+            }
+            i++;
+        }
+
+        return count == 1 ? ans : -1;
+    }
+}
