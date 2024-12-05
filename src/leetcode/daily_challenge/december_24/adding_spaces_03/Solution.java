f4851d5b2ee189b4087501a5212d861d563ffd27
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2024-12-05 06:28:00 +0000

    Solved adding_spaces using bruteforce

--- src/leetcode/daily_challenge/december_24/adding_spaces_03/Solution.java
+++ src/leetcode/daily_challenge/december_24/adding_spaces_03/Solution.java
@@ -1,5 +1,18 @@
 package leetcode.daily_challenge.december_24.adding_spaces_03;
 
 public class Solution {
-    
+    public String addSpaces(String s, int[] spaces) {
+        StringBuffer sb = new StringBuffer();
+        int j = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(j < spaces.length && i == spaces[j]) {
+                sb.append(" ");
+                i--;
+                j++;
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
 }
