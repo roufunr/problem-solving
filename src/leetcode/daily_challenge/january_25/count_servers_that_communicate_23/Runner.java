c25669da72bac570ebc0b25b1e5b745620080214
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2025-01-23 04:14:49 +0000

    Solved count_servers_that_communicate using bfs

---
+++ src/leetcode/daily_challenge/january_25/count_servers_that_communicate_23/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.january_25.count_servers_that_communicate_23;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().countServers(new int[][] { { 1, 0 }, { 0, 1 } });
+    }
+}
