7febb01316acf30420dff88e031c656ccdb1bc24
Abdur Rouf <129791984+roufunr@users.noreply.github.com>
2025-02-01 14:40:42 +0000

    Solved special_array_I using bruteforce

---
+++ src/leetcode/daily_challenge/february_25/special_array_I_01/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.daily_challenge.february_25.special_array_I_01;
+
+public class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
