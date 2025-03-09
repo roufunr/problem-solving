5b397f654703eebac8f3868243b02b151abfc5cc
roufunr <arouf@unr.edu>
2025-03-09 01:44:23 -0500

    Solved alternating_groups_ii_09 using bruteforce

---
+++ src/leetcode/daily_challenge/march_25/alternating_groups_ii_09/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.daily_challenge.march_25.alternating_groups_ii_09;
+
+public class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int[] newColor = new int[colors.length + k - 1];
+        for(int i = 0; i < colors.length; i++) {
+            newColor[i] = colors[i];
+        }
+        for(int i = colors.length; i < newColor.length; i++) {
+            newColor[i] = colors[i - colors.length];
+        }
+        int count = 1;
+        int result = 0;
+        for(int i = 1; i < newColor.length; i++) {
+            if(newColor[i] != newColor[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if(count >= k) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
