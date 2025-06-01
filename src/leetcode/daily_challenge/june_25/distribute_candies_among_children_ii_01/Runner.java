7225061b87e878b86bca438b3b69b465e43e6e9c
roufunr <arouf@unr.edu>
2025-06-01 15:49:09 -0400

    added runner

---
+++ src/leetcode/daily_challenge/june_25/distribute_candies_among_children_ii_01/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.june_25.distribute_candies_among_children_ii_01;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().distributeCandies(5, 2);
+    }
+}
