f8a0423426328d64db0f8b840aeb0f673d02313b
roufunr <rouf@nevada.unr.edu>
2025-07-22 18:58:32 -0400

    Partially solved

---
+++ src/leetcode/daily_challenge/july_25/maximum_erasure_value_22/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.daily_challenge.july_25.maximum_erasure_value_22;
+
+public class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        int left = 0;
+        int currentSum = 0;
+        int maxSum = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            while (seen.contains(nums[right])) {
+                seen.remove(nums[left]);
+                currentSum -= nums[left];
+                left++;
+            }
+            
+            seen.add(nums[right]);
+            currentSum += nums[right];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+}
