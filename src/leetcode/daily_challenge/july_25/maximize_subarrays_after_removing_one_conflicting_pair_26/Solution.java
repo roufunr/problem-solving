c632cc8b68f832ea6ea083dcbc144671aa1f2d9f
roufunr <arouf@unr.edu>
2025-07-27 00:44:03 -0400

    Solved using greedy approach

--- src/leetcode/daily_challenge/july_25/maximize_subarrays_after_removing_one_conflicting_pair_26/Solution.java
+++ src/leetcode/daily_challenge/july_25/maximize_subarrays_after_removing_one_conflicting_pair_26/Solution.java
@@ -1,51 +1,44 @@
 package leetcode.daily_challenge.july_25.maximize_subarrays_after_removing_one_conflicting_pair_26;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
     public long maxSubarrays(int n, int[][] conflictingPairs) {
-        int[] minConflict = new int[n + 1];
-        int[] secondMinConflict = new int[n + 1];
-        Arrays.fill(minConflict, Integer.MAX_VALUE);
-        Arrays.fill(secondMinConflict, Integer.MAX_VALUE);
-
-        for (int[] pair : conflictingPairs) {
+        @SuppressWarnings("unchecked")
+        List<Integer>[] minConflict = new ArrayList[n + 1];
+        for(int i = 0; i < minConflict.length; i++) {
+            minConflict[i] = new ArrayList<>();
+        }
+        for(int[] pair : conflictingPairs) {
             int a = Math.min(pair[0], pair[1]);
             int b = Math.max(pair[0], pair[1]);
-            if (b < minConflict[a]) {
-                secondMinConflict[a] = minConflict[a];
-                minConflict[a] = b;
-            } else if (b < secondMinConflict[a]) {
-                secondMinConflict[a] = b;
+            minConflict[b].add(a);
+        }
+
+        long[] closestConflict = {0, 0};
+        long result = 0;
+        long[] bonus = new long[n + 1];
+        for(int r = 1; r <= n; r++) {
+            for(int maxCon : minConflict[r]) {
+                if(maxCon > closestConflict[0]) {
+                    closestConflict[1] = closestConflict[0];
+                    closestConflict[0] = maxCon;
+                } else if(maxCon > closestConflict[1]) {
+                    closestConflict[1] =  maxCon;
+                }
+            }
+            result += r - closestConflict[0];
+            if(closestConflict[0] > 0) {
+                bonus[(int)closestConflict[0]] += closestConflict[0] - closestConflict[1]; 
             }
         }
-
-        long total = 0;
-        long[] recoverable = new long[n + 1];
-        int currentMinIdx = n;
-        int secondBound = Integer.MAX_VALUE;
-
-        for (int i = n; i >= 1; i--) {
-            if (minConflict[currentMinIdx] > minConflict[i]) {
-                secondBound = Math.min(secondBound, minConflict[currentMinIdx]);
-                currentMinIdx = i;
-            } else {
-                secondBound = Math.min(secondBound, minConflict[i]);
-            }
-
-            total += Math.min(minConflict[currentMinIdx], n + 1) - i;
-
-            recoverable[currentMinIdx] +=
-                Math.min(Math.min(secondBound, secondMinConflict[currentMinIdx]), n + 1)
-                - Math.min(minConflict[currentMinIdx], n + 1);
+        long maxBonus = 0;
+        for(long bon : bonus) {
+            maxBonus = Math.max(bon, maxBonus);
         }
-
-        long maxRecover = 0;
-        for (long val : recoverable) {
-            maxRecover = Math.max(maxRecover, val);
-        }
-
-        return total + maxRecover;
+        return result + maxBonus;
     }
 }
