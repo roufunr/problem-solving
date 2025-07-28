a5506b57e6882c98088e48ae32d98c4cc6157cff
roufunr <rouf@nevada.unr.edu>
2025-07-28 11:21:04 -0400

    Solved using bruteforce

---
+++ src/leetcode/daily_challenge/july_25/count_number_of_maximum_bitwise_or_subsets_28/Runner.java
@@ -0,0 +1,7 @@
+package leetcode.daily_challenge.july_25.count_number_of_maximum_bitwise_or_subsets_28;
+
+public class Runner {
+    public static void main(String[] args) {
+        new Solution().countMaxOrSubsets(new int[]{2, 2, 2});
+    }
+}
