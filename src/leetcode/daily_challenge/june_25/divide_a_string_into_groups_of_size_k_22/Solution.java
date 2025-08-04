ce6c7b55da0c6e2bd58425353ee859af6be93730
Abdur Rouf <abdurroufbd97@gmail.com>
2025-08-04 10:46:57 -0400

    refactored

--- src/leetcode/daily_challenge/june_25/divide_a_string_into_groups_of_size_k_22/Solution.java
+++ src/leetcode/daily_challenge/june_25/divide_a_string_into_groups_of_size_k_22/Solution.java
@@ -1,11 +1,11 @@
-import leetcode.daily_challenge.june_25.divide_a_string_into_groups_of_size_k_22;
+package leetcode.daily_challenge.june_25.divide_a_string_into_groups_of_size_k_22;
 
 public class Solution {
     public String[] divideString(String s, int k, char fill) {
         if(s.length() % k != 0) {
             int remaining = k - (s.length() % k);
             // System.out.println(remaining);
-            StringBuffer x = new StringBuffer(s);
+            StringBuilder x = new StringBuilder(s);
             for(int i = 0; i < remaining; i++) {
                 x.append(fill);
             }
