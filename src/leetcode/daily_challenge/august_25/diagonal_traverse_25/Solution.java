0530a0ae2567ef09a8024dddd5a4bba5818f211f
roufunr <rouf@nevada.unr.edu>
2025-08-25 19:50:22 -0400

    Solved using diagonal traversal

---
+++ src/leetcode/daily_challenge/august_25/diagonal_traverse_25/Solution.java
@@ -0,0 +1,54 @@
+package leetcode.daily_challenge.august_25.diagonal_traverse_25;
+
+public class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        boolean upward = true;
+        int i = 0;
+        int j = 0;
+        int rows = mat.length;
+        int columns = mat[0].length;
+        int[] ans = new int[rows * columns];
+        int k = 0;
+        while(true) {
+            ans[k++] = mat[i][j];
+            if(i == rows - 1 && j == columns - 1) {
+                break;
+            }
+            if(upward) {
+                i--;
+                j++;
+            } else {
+                i++;
+                j--;
+            }
+            boolean case1 = i < 0;
+            boolean case2 = j >= columns;
+            boolean case3 = j < 0;
+            boolean case4 = i >= rows;
+
+            if(case1 || case2 || case3 || case4) {
+                if(upward) {
+                    i++;
+                    j--;
+
+                    if(case1 && j + 1 < columns) {
+                        j++;
+                    } else if (case2 && i + 1 < rows) {
+                        i++;
+                    }
+                } else {
+                    i--;
+                    j++;
+
+                    if(case3 && i + 1 < rows) {
+                        i++;
+                    } else if (case4 && j + 1 < columns) {
+                        j++;
+                    }
+                }
+                upward = upward == false ? true : false;
+            }
+        }
+        return ans;
+    }
+}
